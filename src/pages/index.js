d485a24e008ee3d7bb83bed2e1db0425a3b6f8fa
Telos <Cyber21fine@protonmail.com>
2021-08-12 23:34:37 +0300

    Tailwind.css Next.js Start

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CartPeek</title>
+      </Head>
+    <Navbar/>
+    <h3>Hello Telos</h3>
+    
+    </div>
+  );
+}
