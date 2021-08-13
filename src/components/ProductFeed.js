5339caae485cfab813024929c36d5305a8b91f45
Telos <Cyber21fine@protonmail.com>
2021-08-13 23:58:27 +0300

    HomePage (1/2)

---
+++ src/components/ProductFeed.js
@@ -0,0 +1,11 @@
+function ProductFeed({ products }) {
+  return (
+    <div>
+      {products.map((products) => (
+        <p>{products.title}</p>
+      ))}
+    </div>
+  );
+}
+
+export default ProductFeed;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,35 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({products}) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>CartPeek</title>
       </Head>
     <Navbar/>
-    <h3>Hello Telos</h3>
+    <main className="max-w-screen-2xl mx-auto">
     
+    <Banner/>
+
+    <ProductFeed products={products}/>
+
+    </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context){
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json() 
+    );
+
+    return {
+      props: {
+        products
+      }
+    }
+
 }
